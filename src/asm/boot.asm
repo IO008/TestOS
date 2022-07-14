ac8f9e91df285fe9f3a9a2d0fc719f9d32cff8db
Shrek <togo33937@gmail.com>
2022-07-14 16:15:57 +0800

    refactor

--- src/asm/boot.asm
+++ src/asm/boot.asm
@@ -1,98 +1,51 @@
-mov ax,0xb800                 ;指向文本模式的显示缓冲区
-mov es,ax
+         jmp near start
+         
+  mytext db 'L',0x07,'a',0x07,'b',0x07,'e',0x07,'l',0x07,' ',0x07,'o',0x07,\
+            'f',0x07,'f',0x07,'s',0x07,'e',0x07,'t',0x07,':',0x07
+  number db 0,0,0,0,0
+  
+  start:
+         mov ax,0x7c0                  ;设置数据段基地址 
+         mov ds,ax
+         
+         mov ax,0xb800                 ;设置附加段基地址 
+         mov es,ax
+         
+         cld
+         mov si,mytext                 
+         mov di,0
+         mov cx,(number-mytext)/2      ;实际上等于 13
+         rep movsw
+     
+         ;得到标号所代表的偏移地址
+         mov ax,number
+         
+         ;计算各个数位
+         mov bx,ax
+         mov cx,5                      ;循环次数 
+         mov si,10                     ;除数 
+  digit: 
+         xor dx,dx
+         div si
+         mov [bx],dl                   ;保存数位
+         inc bx 
+         loop digit
+         
+         ;显示各个数位
+         mov bx,number 
+         mov si,4                      
+   show:
+         mov al,[bx+si]
+         add al,0x30
+         mov ah,0x04
+         mov [es:di],ax
+         add di,2
+         dec si
+         jns show
+         
+         mov word [es:di],0x0744
 
-;以下显示字符串"Label offset:"
-mov byte [es:0x00],'L'
-mov byte [es:0x01],0x07
-mov byte [es:0x02],'a'
-mov byte [es:0x03],0x07
-mov byte [es:0x04],'b'
-mov byte [es:0x05],0x07
-mov byte [es:0x06],'e'
-mov byte [es:0x07],0x07
-mov byte [es:0x08],'l'
-mov byte [es:0x09],0x07
-mov byte [es:0x0a],' '
-mov byte [es:0x0b],0x07
-mov byte [es:0x0c],"o"
-mov byte [es:0x0d],0x07
-mov byte [es:0x0e],'f'
-mov byte [es:0x0f],0x07
-mov byte [es:0x10],'f'
-mov byte [es:0x11],0x07
-mov byte [es:0x12],'s'
-mov byte [es:0x13],0x07
-mov byte [es:0x14],'e'
-mov byte [es:0x15],0x07
-mov byte [es:0x16],'t'
-mov byte [es:0x17],0x07
-mov byte [es:0x18],':'
-mov byte [es:0x19],0x07
+         jmp near $
 
-mov ax,number                 ;取得标号number的偏移地址
-mov bx,10
-
-;设置数据段的基地址
-mov cx,cs
-mov ds,cx
-
-;求个位上的数字
-mov dx,0
-div bx
-mov [0x7c00+number+0x00],dl   ;保存个位上的数字
-
-;求十位上的数字
-xor dx,dx
-div bx
-mov [0x7c00+number+0x01],dl   ;保存十位上的数字
-
-;求百位上的数字
-xor dx,dx
-div bx
-mov [0x7c00+number+0x02],dl   ;保存百位上的数字
-
-;求千位上的数字
-xor dx,dx
-div bx
-mov [0x7c00+number+0x03],dl   ;保存千位上的数字
-
-;求万位上的数字 
-xor dx,dx
-div bx
-mov [0x7c00+number+0x04],dl   ;保存万位上的数字
-
-;以下用十进制显示标号的偏移地址
-mov al,[0x7c00+number+0x04]
-add al,0x30
-mov [es:0x1a],al
-mov byte [es:0x1b],0x04
-
-mov al,[0x7c00+number+0x03]
-add al,0x30
-mov [es:0x1c],al
-mov byte [es:0x1d],0x04
-
-mov al,[0x7c00+number+0x02]
-add al,0x30
-mov [es:0x1e],al
-mov byte [es:0x1f],0x04
-
-mov al,[0x7c00+number+0x01]
-add al,0x30
-mov [es:0x20],al
-mov byte [es:0x21],0x04
-
-mov al,[0x7c00+number+0x00]
-add al,0x30
-mov [es:0x22],al
-mov byte [es:0x23],0x04
-
-mov byte [es:0x24],'D'
-mov byte [es:0x25],0x07
-
-infi: jmp near infi                 ;无限循环
-
-number db 0,0,0,0,0
-
-times 203 db 0
-        db 0x55,0xaa
+  times 510-($-$$) db 0
+                   db 0x55,0xaa
